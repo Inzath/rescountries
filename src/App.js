595dd378b7e7ec0e9197b2dea7fb6f77fb9dfad4
Inzath <inzathmonteiro@gmail.com>
2022-08-16 19:13:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
+/*const App = () => {
+  return (
+    <div>
+      <h1>Hello la famille react</h1>
+    </div>
+  );
+};*/
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+         <Route path = "/" element={<Home />} />
+         <Route path = "/about" element={<About />} />
+      </Routes> 
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App; */
+
+
---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br></br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga aliquid sed numquam provident minus eum. Nam quis quaerat dolore vero inventore. Quasi neque eos fugiat modi voluptatem blanditiis facere nulla, dicta officia a consequatur illo natus. Vel repellendus velit, minus eum assumenda distinctio nam voluptates tempora nihil qui repellat dolores.</p>
+            <br></br>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima totam deserunt dignissimos aperiam expedita eius fugiat voluptatem! Quibusdam, dolorem excepturi.</p>
+        </div>
+    );
+};
+
+export default About;
